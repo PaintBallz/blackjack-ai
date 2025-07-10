4dc03cbfc5cd2ee81cff32d70bf0da5b71b105f0
Nicholas Borrelli <nicholasborrelli@proton.me>
2025-07-10 19:39:08 -0400

    7.10.25

---
+++ PlayerClass.py
@@ -0,0 +1,34 @@
+
+
+"""
+Blackjack Player Class
+"""
+class Player:
+
+    # Constructor
+    def __init__(self, initialchip: int):
+        self.ChipCount = initialchip
+
+    # Hit action
+    def Hit(self):
+        pass
+
+    # Stay action
+    def Stay(self):
+        pass
+
+    # Double action
+    def Double(self):
+        pass
+
+    # Split action
+    def Split(self):
+        pass
+
+    # Calculate Sum
+    def GetSum(self):
+        pass
+
+    # Chip count
+    def GetChipCoint(self):
+        pass
